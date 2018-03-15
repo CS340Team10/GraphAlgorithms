a5c17fc68a1b27bffe35b75a6387119b3d94f161
Ephraim Kunz <ephraimkunz@me.com>
2018-03-15 07:34:59 -0600

    Initial commint

---
+++ src/DestCard.java
@@ -0,0 +1,14 @@
+
+public class DestCard {
+    public String startCity;
+    public String endCity;
+    public int pointValue;
+
+    public DestCard(String startCity, String endCity, int pointValue) {
+        this.startCity = startCity;
+        this.endCity = endCity;
+        this.pointValue = pointValue;
+    }
+    
+    
+}
---
+++ src/TrainCard.java
@@ -0,0 +1,14 @@
+
+public class TrainCard {
+	enum Colors {
+		wildcard,
+		white,
+		yellow,
+		blue,
+		green,
+		purple,
+		orange,
+		red,
+		black
+	}
+}
